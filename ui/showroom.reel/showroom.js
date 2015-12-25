0ca120cdd8f92a3d9752c3a9560893b816b57441
logicerpsolution <noreply>
2015-12-25 07:59:18 +0000

    Add component showroom

---
+++ ui/showroom.reel/showroom.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/showroom.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Showroom
+ * @extends Component
+ */
+exports.Showroom = Component.specialize(/** @lends Showroom# */ {
+    constructor: {
+        value: function Showroom() {
+            this.super();
+        }
+    }
+});
